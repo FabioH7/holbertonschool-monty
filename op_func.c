81187ec3c9e524aedf8d983f2ec08dbbe23d63d6
Katrolli <ennio_cano@live.com>
2022-12-21 07:35:18 -0800

    adding for fabio

--- op_func.c
+++ op_func.c
@@ -60,7 +60,7 @@ void add (stack_t **stack, unsigned int line_number)
 {
 	int sum = 0;
 
-	if ((*stack)->next == NULL)
+	if ((*stack)->next == NULL || *stack == NULL)
 	{
 		fprintf(stderr,"L%i: can't add, stack too short\n", line_number);
                 exit(EXIT_FAILURE);
